a62a4acbbb7c6e2239fae48cb4c758979c2e7ef6
UserStudios1 <lausitzkind52@gmail.com>
2024-03-22 14:29:55 +0100

    Added Loop to set ammo to new Ammo Value after change

--- main.cpp
+++ main.cpp
@@ -5,42 +5,32 @@
 
 
 int main() {
-	// Get Process ID of the target process
 	DWORD procId = GetProcId(L"ac_client.exe");
 
-	// GetModuleBaseAddress
 	uintptr_t moduleBase = GetModuleBaseAddress(procId, L"ac_client.exe");
 
-	// Get Handle
 	HANDLE hProcess = 0;
 	hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
 
-	// Resolve Base Address of the pointer chain
 	uintptr_t dynamicPtrBaseAddr = moduleBase + 0x17E0A8;
 
 	std::cout << "DynamicPtrBaseAddr = " << "0x" << std::hex << std::uppercase << dynamicPtrBaseAddr << std::endl;
 
-	// Resolve ammo pointer chain
-	std::vector<unsigned int> ammoOffsets = {0x364, 0x14, 0x0};
+	std::vector<unsigned int> ammoOffsets = { 0x364, 0x14, 0x0 };
 	uintptr_t ammoAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddr, ammoOffsets);
 
 	std::cout << "ammoAddr = " << "0x" << std::hex << std::uppercase << ammoAddr << std::endl;
 
-	// Read Ammo Value
-	int ammoValue = 0;
-	ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
+	while (true) {
+		int ammoValue = 0;
+		ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
 
-	std::cout << "Current Ammo = " << std::dec << ammoValue << std::endl;
+		int newAmmo = 1337;
+		if (ammoValue != newAmmo) {
+			WriteProcessMemory(hProcess, (BYTE*)ammoAddr, &newAmmo, sizeof(newAmmo), nullptr);
+		}
 
-	// Write to it
-	int newAmmo = 1337;
-	WriteProcessMemory(hProcess, (BYTE*)ammoAddr, &newAmmo, sizeof(newAmmo), nullptr);
-
-	// Read out again
-	ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
-
-	std::cout << "New ammo = " << std::dec << ammoValue << std::endl;
-
-	getchar();
+		ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
+	}
 	return 0;
 }
